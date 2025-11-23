48d1c37536ce7f6e85f1a2cb7ed391d07c479477
yuudev14 <takaki.yu.dev@gmail.com>
2025-11-23 22:25:18 +0900

    add response Object

--- api/chat_completion/versions/v1.go
+++ api/chat_completion/versions/v1.go
@@ -3,9 +3,11 @@ package chatcompletionv1
 import (
 	"encoding/json"
 	"io"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/yuudev14/ai-gateway/internal/llm"
+	rest "github.com/yuudev14/ai-gateway/internal/rests"
 )
 
 type ChatCompletionController struct {
@@ -17,18 +19,25 @@ func NewChatCompletionController() *ChatCompletionController {
 
 func (c *ChatCompletionController) ChatCompletionProxy(ctx *gin.Context) {
 	var data map[string]interface{}
+	response := rest.Response{C: ctx}
 	reqBody, _ := io.ReadAll(ctx.Request.Body)
 
 	if err := json.Unmarshal(reqBody, &data); err != nil {
-		panic(err)
+		return
 	}
+
 	stream, ok := data["stream"]
+
+	// if stream does not exist in the data, set stream to false
 	if !ok {
 		stream = false
 	}
 
 	if stream == true {
-		llm.StreamChatCompletion(ctx, string(reqBody))
+		err := llm.StreamChatCompletion(ctx, string(reqBody))
+		if err != nil {
+			response.ResponseError(http.StatusBadRequest, err.Error())
+		}
 		return
 	}
 	llm.ChatCompletion(ctx, string(reqBody))
